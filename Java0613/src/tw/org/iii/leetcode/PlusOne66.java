2c7be8118d56dbd2e9da823f73f60f494d736123
User <User@WS10.r1802.tciii.org.tw>
2023-08-03 17:02:39 +0800

    08030502

---
+++ Java0613/src/tw/org/iii/leetcode/PlusOne66.java
@@ -0,0 +1,15 @@
+package tw.org.iii.leetcode;
+
+public class PlusOne66 {
+Solution test = new Solution();
+	public static void main(String[] args) {
+		PlusOne66 t1 = new PlusOne66();
+		t1.test.plusOne(new int[] {1,2});
+	}
+
+	private class Solution {
+		public int[] plusOne(int[] digits) {
+			return new int[] { 1 };
+		}
+	}
+}
